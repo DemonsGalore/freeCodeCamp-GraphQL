0103ec19d5c64e564cdf4d749e64d07efb62a3bc
Andreas Stopat <dev@demonsgalore.de>
2019-03-19 16:31:19 +0100

    add component, index.css; setup apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import BookList from './components/BookList';
+
 import './App.css';
+
+
+// apollo client initialization
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <main>
+            <h1>HELLO</h1>
+            <BookList />
+          </main>
+        </div>
+      </ApolloProvider>
     );
   }
 }
